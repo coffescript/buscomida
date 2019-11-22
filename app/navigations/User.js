c7238b0f112cc43aea662fcbd5b0afc0a2afdbba
coffescript <dmejia1204@gmail.com>
2019-11-21 20:00:14 -0600

    create of navigation bar

---
+++ app/navigations/User.js
@@ -0,0 +1,117 @@
+import React from 'react'
+import { Icon } from 'react-native-elements'
+
+import { createAppContainer } from 'react-navigation'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { createStackNavigator } from 'react-navigation-stack'
+
+import HomeScreen from '../screens/Home';
+import TopFiveScreen from '../screens/TopFive'
+import SearchScreen from '../screens/Search'
+import MyAccountScreen from '../screens/MyAccount'
+
+const homeScreenStack = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Home'
+    })
+  }
+});
+
+const topFiveScreenStack = createStackNavigator({
+  TopFive: {
+    screen: TopFiveScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Top 5 Restaurants'
+    })
+  }
+})
+
+const searchScreenStack = createStackNavigator({
+  Search: {
+    screen: SearchScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Search'
+    })
+  }
+})
+
+const myAccountScreenStack = createStackNavigator({
+  MyAccount: {
+    screen: MyAccountScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: 'My Account'
+    })
+  }
+})
+
+const RootStack = createBottomTabNavigator(
+  {
+    Home: {
+      screen: homeScreenStack,
+      navigationOptions: ({ navigation }) => ({
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ tintColor }) => (
+          <Icon
+            name="compass-outline"
+            type="material-community"
+            size={22}
+            color={tintColor}
+          />
+        )
+      })
+    },
+    TopFive: {
+      screen: topFiveScreenStack,
+      navigationOptions: ({ navigation }) => ({
+        tabBarLabel: "Top 5",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon
+            name="star-outline"
+            type="material-community"
+            size={22}
+            color={tintColor}
+          />
+        )
+      })
+    },
+    Search: {
+      screen: searchScreenStack,
+      navigationOptions: ({ navigation }) => ({
+        tabBarLabel: "Search",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon
+            name="magnify"
+            type="material-community"
+            size={22}
+            color={tintColor}
+          />
+        )
+      })
+    },
+    MyAccount: {
+      screen: myAccountScreenStack,
+      navigationOptions: ({ navigation }) => ({
+        tabBarLabel: "My Account",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon
+            name="account-settings"
+            type="material-community"
+            size={22}
+            color={tintColor}
+          />
+        )
+      })
+    }
+  },
+  {
+    initialRouteName: "MyAccount",
+    tabBarOptions: {
+      inactiveTintColor: "#646464",
+      activeTintColor: "#00a680"
+    }
+  }
+);
+
+export default createAppContainer(RootStack)
---
+++ app/utils/Firebase.js
@@ -0,0 +1,11 @@
+ // Your web app's Firebase configuration
+ export default firebaseConfig = {
+  apiKey: "AIzaSyDSsHDSV2T19eVLodDQsvYcJk3UA93-0C4",
+  authDomain: "buscomida-5c2ce.firebaseapp.com",
+  databaseURL: "https://buscomida-5c2ce.firebaseio.com",
+  projectId: "buscomida-5c2ce",
+  storageBucket: "buscomida-5c2ce.appspot.com",
+  messagingSenderId: "1034607790966",
+  appId: "1:1034607790966:web:837f6f27a6699af9208857",
+  measurementId: "G-DBQN339BK2"
+}
